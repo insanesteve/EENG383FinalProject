87d170e2f6f4ab720160cae2e6e07d2c81961233
Easton Bornemeier <erbornemeier@mines.edu>
2018-12-04 19:35:00 -0700

    Thank god it works

--- LockOpener.X/main.c
+++ LockOpener.X/main.c
@@ -3,6 +3,7 @@
 #define NUM_STEPS 3200
 #define STEPS_PER_NUM 80
 #define FULL_ROTATION 40
+#define READ_TIME 300 //ms
 #define CCW 0
 #define CW  1
 
@@ -11,7 +12,7 @@ bool openLock(void);
 void spinStepper(int16_t nums);
 void delay(uint16_t ms);
 void moveLockTo(uint8_t pos, uint8_t dir);
-void enterCombo(uint8_t a, uint8_t b, uint8_t c);
+uint8_t enterCombo(uint8_t a, uint8_t b, uint8_t c, uint8_t v);
 
 uint8_t lockPosition = 0;
 
@@ -20,12 +21,12 @@ void main(void)
     // Initialize the device
     SYSTEM_Initialize();
     DIR_SetHigh();
-    ENABLE_SetHigh();
+    //ENABLE_SetHigh();
+    ENABLE_SetLow();
     
-    uint8_t lastNum;
+    uint8_t first = 0, second = 0;
 
-    /*EXAMPLE OF STEPPER OPERATION*/
-    while (1){
+    while (true){
         
         while (!EUSART1_DataReady);
         char cmd = EUSART1_Read();
@@ -36,17 +37,20 @@ void main(void)
                 printf("Welcome to the Lock PIC\r\n");
                 printf("-----------------------\r\n");
                 printf("s: input the correct combo and open the lock!\r\n");
+                printf("l: loop through last digit combos\r\n");
                 printf("o: open da thing!\r\n");
                 printf("r: is da thing open????s\r\n");
-                printf("k: reset lock position to 0");
+                printf("k: reset lock position to 0\r\n");
+                
                 printf("\r\n");
                 break;
             
             case 's':
-                
-                enterCombo(14,20,2);
-                
+                enterCombo(14,20,2,true);
                 break;
+            case 'l':
+                //enterCombo(10,30,40,true);
+                enterCombo(14,20,40,true);
             case 'o':
                 openLock();
                 break;
@@ -56,52 +60,89 @@ void main(void)
             case 'k':
                 lockPosition = 0;
                 break;
+            case 't':
+                ENABLE_Toggle();
+                break;
+            case '1':
+                enterCombo(14,20,2,true);
+                break;
+            case '2':
+                if (enterCombo(4,7,9,true)) break;
+                if (enterCombo(10,20,5,true)) break;
+                if (enterCombo(30,10,2,true)) break;
+                if (enterCombo(14,20,2,true)) break;
+                if (enterCombo(1,2,3,true)) break;
+                break;
+            case '3':
+                break;
+                
+                
         }
     }
 }
 
-void enterCombo(uint8_t a, uint8_t b, uint8_t c){
-    uint8_t lastNum;
+uint8_t enterCombo(uint8_t a, uint8_t b, uint8_t c, uint8_t v){
+    int8_t lastNum;
+    if (v) printf("Trying %u %u %u\r\n", a, b, c);
+    //enter first number
     spinStepper(-2*FULL_ROTATION);
     moveLockTo(a, CW);
-    printf("Now at position %u\r\n", lockPosition);
     delay(20);
 
+    //enter second number
     spinStepper(1*FULL_ROTATION);
     moveLockTo(b, CCW);
-    printf("Now at position %u\r\n", lockPosition);
     delay(20);
-
-    for(lastNum = 39; lastNum != 0; lastNum--){
-        moveLockTo(lastNum, CW);
-        printf("Now at position %u\r\n", lockPosition);
-        delay(20);
-        if (openLock()) break;
+    
+    if (c != 40){
+        moveLockTo(c, CW);
+        return openLock();
+    }
+    else {
+        //try all variations of the last number 
+        for(lastNum = b-3; lastNum != b; lastNum -= 3){
+            if (lastNum < 0) lastNum += FULL_ROTATION;
+            moveLockTo(lastNum, CW);
+            printf("Now at position %u\r\n", lockPosition);
+            delay(20);
+            if (openLock()) return true;
+        }
+        return false;
     }
 
 }
 
 void moveLockTo(uint8_t pos, uint8_t dir){
+    //determine difference between current pos and target pos
     int16_t delta;
     if (dir == CW)
         delta = -((lockPosition + FULL_ROTATION - pos) % FULL_ROTATION);
     else
         delta = ((pos - lockPosition + FULL_ROTATION) % FULL_ROTATION);
+    
+    //spin the stepper according to difference
     spinStepper(delta);
+    
+    //update the lock position
     if (delta < 0) delta += FULL_ROTATION;
     lockPosition = (lockPosition + delta) % FULL_ROTATION;
    
 }
 
 void spinStepper(int16_t nums){
-    ENABLE_SetLow();
     uint16_t i;
+    
+    //turn the stepper on
+    ENABLE_SetLow();
+    
+    //determine direction
     DIR_SetHigh();
     if (nums < 0){
         DIR_SetLow();
         nums = -nums;
     }
     
+    //spin stepper for nums steps
     for (i = 0; i < nums*STEPS_PER_NUM; i++){
             STEP_SetHigh();
             STEP_SetLow();
@@ -109,20 +150,32 @@ void spinStepper(int16_t nums){
             TMR1_Reload();
             while(!TMR1_HasOverflowOccured());
     }
-    ENABLE_SetHigh();
+    
+    //turn the stepper off
+    //ENABLE_SetHigh();
 }
 
 bool openLock(void){
     uint8_t opened = false;
     uint16_t i;
+    
+    //fire the solenoid
     OPENER_SetHigh();
-    for (i=0; i < 1000; i++){
+    
+    //hold it open until we get a read, if any
+    for (i=0; i < READ_TIME; i++){
         delay(1);
-        if (readSwitch()) opened = true;
+        if (readSwitch()){ 
+            opened = true;
+        }
     }
+    
     if(opened) printf("OPENED!\r\n");
+    
+    //turn off the solenoid
     OPENER_SetLow();
     delay(50);
+    
     return opened;
 }
 
@@ -132,8 +185,7 @@ bool readSwitch(void){
 
 
 void delay(uint16_t ms){
-    uint16_t i = 0;
-    // 1 ms per loop
+    uint16_t i;
     for (i = 0; i < ms; i++){
         INTCONbits.TMR0IF = 0;
         TMR0_Reload();
